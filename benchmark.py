04e8ae9b352b1d7f80b8d7b72df7a24cfd300e91
Nicholas Herson <nicholas.herson@berkeley.edu>
2013-05-01 23:44:10 -0700

    better benchmarking

--- benchmark.py
+++ benchmark.py
@@ -3,20 +3,19 @@ import kenken
 import os
 import sys
 
+
+#Globals for StandardOutput and DevNull files for writing to
+#These toggle print statements essentially
 stdout = sys.stdout
 devnull = open(os.devnull, 'w')
+puzzleDir = "puzzles/"
 
 def beginFullBenchmark():
     """
     Solves a series of KenKen puzzles, and times them, giving the times
     as puzzles are solved.
     """
-    #makes it so nothing gets printed from the normal operation of the program
-    #redirects all prints to /dev/null, meaning they just go to nothing 
-    #we will change this back once we want to report the timings
     
-    puzzleDir = "puzzles/"
-
     #FOLLOW THIS FORMAT FOR ADDING TESTS TO THIS BENCHMARKING FILE
     currentPuzzle = "nyt6x6_05-01.kk"
     printing(False)
@@ -25,8 +24,22 @@ def beginFullBenchmark():
     stopTime = time()
     printing(True)
     print(currentPuzzle + " solved in " + str(stopTime - startTime) + " seconds")
+
+def beginSingleBenchmark(filename):
+    printing(False)
+    startTime = time()
+    kenken.main(puzzleDir + filename)
+    stopTime = time()
+    printing(True)
+    print(filename + " solved in " + str(stopTime - startTime) + " seconds")
+
+ 
     
 def printing(b):
+    """
+    If True, enables printing
+    If False, disables printing (printing gets sent to nothingness... the great null)
+    """
     if b:
         sys.stdout = stdout
     else:
@@ -35,4 +48,10 @@ def printing(b):
 
     
 if __name__ == "__main__":
+    try:
+        if (sys.argv[1]):
+            beginSingleBenchmark(sys.argv[1])
+            exit()
+    except IndexError:
+        pass
     beginFullBenchmark()
